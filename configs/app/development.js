b828958033dc5416d88aca9ef8d3aadcb010c7ff
minung.han <mark.han@goorm.io>
2021-04-13 09:13:50 +0000

    init project

---
+++ configs/app/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+  keys: {
+    kakaoWork: {
+      bot: '',
+    },
+  },
+};
---
+++ libs/kakaoWork/index.js
@@ -0,0 +1,32 @@
+const Config = require('config');
+
+const axios = require('axios');
+const kakaoInstance = axios.create({
+  baseURL: 'https://api.kakaowork.com',
+  headers: {
+    Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
+  },
+});
+
+exports.getUserList = async () => {
+  const res = await kakaoInstance.get('/v1/users.list');
+  return res.data.users;
+};
+
+exports.openConversations = async ({ userId }) => {
+  const data = {
+    user_id: userId,
+  };
+  const res = await kakaoInstance.post('/v1/conversations.open', data);
+  return res.data.conversation;
+};
+
+exports.sendMessage = async ({ conversationId, text, blocks }) => {
+  const data = {
+    conversation_id: conversationId,
+    text,
+    ...blocks && { blocks },
+  };
+  const res = await kakaoInstance.post('/v1/messages.send', data);
+  return res.data.message;
+};
